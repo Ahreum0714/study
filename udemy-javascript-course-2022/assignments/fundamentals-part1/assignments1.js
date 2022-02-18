c5634adb05adad7fcb1a8784a65addd9702c059c
arh714 <yalin7145@gmail.com>
2022-02-18 21:05:01 +0900

    values and variables

---
+++ udemy-javascript-course-2022/assignments/fundamentals-part1/assignments1.js
@@ -0,0 +1,13 @@
+/* Lecture: Values and Variables */
+
+// 1. Declare variables called 'country', 'continent' and 'population' and 
+// assign their values according to your own country (population in millions)
+// 2. Log their values to the console 
+
+let country = 'korea';
+let continent = 'asia';
+let population = '51780000';
+
+console.log(country);
+console.log(continent);
+console.log(population);
