1b3e47a94456c71e3549c098f9edd325ec460140
Ahreum0714 <yalin7145@gmail.com>
2022-02-22 01:35:49 +0900

    [Subject] assignments5 - Strings and Template Literals
    [Description]
    done and correct
    
    [Issue]

--- udemy-javascript-course-2022/assignments/fundamentals-part1/assignments5.js
+++ udemy-javascript-course-2022/assignments/fundamentals-part1/assignments5.js
@@ -2,3 +2,28 @@
 
 // 1. Recreate the 'description' variable from the last assignment, this time
 // using the template literal syntax
+
+let country = 'korea';
+let continent = 'asia';
+const motherLanguage = 'Korean';
+let koreaPopulation = 51;
+
+//1
+console.log(koreaPopulation / 2);
+//2
+koreaPopulation++;
+console.log(koreaPopulation);
+//3
+let finlandPopulation = 6;
+let isLarger = koreaPopulation > finlandPopulation;
+console.log(isLarger);
+//4
+let averagePopulation = 33;
+let isLesser = koreaPopulation < averagePopulation;
+console.log(isLesser);
+//5
+let description = country + ' is in ' + continent + ', and its ' + koreaPopulation + ' million people speak ' + motherLanguage;
+console.log(description);
+
+description = `${country} is in ${continent}, and its ${koreaPopulation} million people speak ${motherLanguage}`;
+console.log(description);
