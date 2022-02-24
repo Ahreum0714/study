7082463f5963445a06c925978a01e430d17fcc61
Ahreum0714 <yalin7145@gmail.com>
2022-02-24 23:39:09 +0900

    fundamentals part2 - assignment#4
    Functions Calling Other Functions

--- udemy-javascript-course-2022/assignments/fundamentals-part2/script.js
+++ udemy-javascript-course-2022/assignments/fundamentals-part2/script.js
@@ -2,51 +2,75 @@
 
 /**LECTURE: Functions */
 function describeCountry(country, population, capitalCity) {
-    return `${country} has ${population} million people and its capital city is ${capitalCity}`;
+    return `${country} has ${population} million people and its capital city is ${capitalCity}.`;
 }
 
-const korea = describeCountry('korea', 51, 'seoul');
-const china = describeCountry('china', 100, 'beijing');
-const japan = describeCountry('japan', 70, 'tokyo');
+const descKorea = describeCountry('Korea', 51, 'seoul');
+const descChina = describeCountry('China', 100, 'beijing');
+const descJapan = describeCountry('Japan', 70, 'tokyo');
+console.log(descKorea, descChina, descJapan);
 
-console.log(korea);
-console.log(china);
-console.log(japan);
 
 
 /**LECTURE: Function Declarations vs. Expressions*/
 const worldPopulation = 7900
-const canada = 80;
-const france = 30;
-const usa = 1000;
 
-// function declarations
+// function declarations //
 function percentageOfWorld1(population) {
     return (population / worldPopulation) * 100;
 }
-const countryPopul1 = percentageOfWorld1(canada);
-const countryPopul2 = percentageOfWorld1(france);
-const countryPopul3 = percentageOfWorld1(usa);
 
-console.log(countryPopul1);
-console.log(countryPopul2);
-console.log(countryPopul3);
+const perc1 = percentageOfWorld1(80);
+const perc2 = percentageOfWorld1(30);
+const perc3 = percentageOfWorld1(1000);
+console.log(perc1, perc2, perc3);
 
-// function expressions
+// function expressions //
 const percentageOfWorld2 = function (population) {
     return (population / worldPopulation) * 100;
 }
-const popul1 = percentageOfWorld2(canada);
-const popul2 = percentageOfWorld2(france);
-const popul3 = percentageOfWorld2(usa);
 
-console.log(popul1);
-console.log(popul2);
-console.log(popul3);
+const popul1 = percentageOfWorld2(80);
+const popul2 = percentageOfWorld2(30);
+const popul3 = percentageOfWorld2(1000);
+console.log(popul1, popul2, popul3);
+
 
 
 /**LECTURE: Arrow Functions */
 const percentageOfWorld3 = population => (population / worldPopulation) * 100;
 
+const percPortugal = percentageOfWorld3(10);
+const percChina = percentageOfWorld3(1441);
+const percUSA = percentageOfWorld3(332);
+console.log(percPortugal, percChina, percUSA);
 
-/** */
+
+/**LECTURE: Functions Calling Other Functions */
+function describePopulation(country, population) {
+    const percentage = percentageOfWorld1(population);
+    const description = `${country} has ${population} million people, which is about ${percentage} of the world.`;
+    console.log(description);
+}
+describePopulation('China', 1441);
+describePopulation('USA', 332);
+describePopulation('Portugal', 10);
+
+
+
+/**LECTURE: Introduction to Arrays */
+
+
+
+
+/**LECTURE: Basic Array Operations (Methods)*/
+
+
+
+
+/**LECTURE: Introduction to Objects */
+
+
+
+
+/**LECTURE: Dot vs. Bracket Notation */
