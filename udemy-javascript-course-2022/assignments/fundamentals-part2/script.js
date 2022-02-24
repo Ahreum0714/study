a98418676ff88eb71ad790ef7823c9590f0d4e7f
Ahreum0714 <yalin7145@gmail.com>
2022-02-24 23:16:51 +0900

    fundamentals part2 - assignment#2
    function declarations vs. expressions

--- udemy-javascript-course-2022/assignments/fundamentals-part2/script.js
+++ udemy-javascript-course-2022/assignments/fundamentals-part2/script.js
@@ -15,3 +15,33 @@ console.log(japan);
 
 
 /**LECTURE: Function Declarations vs. Expressions*/
+const worldPopulation = 7900
+const canada = 80;
+const france = 30;
+const usa = 1000;
+
+// function declarations
+function percentageOfWorld1(population) {
+    return (population / worldPopulation) * 100;
+}
+const countryPopul1 = percentageOfWorld1(canada);
+const countryPopul2 = percentageOfWorld1(france);
+const countryPopul3 = percentageOfWorld1(usa);
+
+console.log(countryPopul1);
+console.log(countryPopul2);
+console.log(countryPopul3);
+
+// function expressions
+const percentageOfWorld2 = function (population) {
+    return (population / worldPopulation) * 100;
+}
+const popul1 = percentageOfWorld2(canada);
+const popul2 = percentageOfWorld2(france);
+const popul3 = percentageOfWorld2(usa);
+
+console.log(popul1);
+console.log(popul2);
+console.log(popul3);
+
+/**LECTURE: Arrow Functions */
