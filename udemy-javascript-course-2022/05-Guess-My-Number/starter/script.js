0584d1b94d519d7d0b158319283cab267550df67
Ahreum0714 <yalin7145@gmail.com>
2022-02-26 19:06:39 +0900

    DOM - challenge#1 ('.number' textContent reset)

--- udemy-javascript-course-2022/05-Guess-My-Number/starter/script.js
+++ udemy-javascript-course-2022/05-Guess-My-Number/starter/script.js
@@ -12,7 +12,7 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector(".number").textContent = secretNumber;
+// document.querySelector(".number").textContent = secretNumber;
 
 document.querySelector(".check").addEventListener("click", function () {
   let guess = Number(document.querySelector(".guess").value);
@@ -60,7 +60,8 @@ document.querySelector(".again").addEventListener("click", function () {
   document.querySelector(".score").textContent = score;
   document.querySelector(".number").textContent = secretNumber;
   document.querySelector(".message").textContent = "Start guessing...";
-  document.querySelector(".guess").value = null;
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = "";
   document.querySelector("body").style.backgroundColor = "#222";
   document.querySelector(".number").style.width = "15rem";
 });
