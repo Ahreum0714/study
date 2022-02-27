966a2e79de42d96b81deb60121a070ea7e094b79
arh714 <yalin7145@gmail.com>
2022-02-27 22:34:28 +0900

    DOM - proj#3 Pig Game (resetting the game)

--- udemy-javascript-course-2022/07-Pig-Game/starter/script.js
+++ udemy-javascript-course-2022/07-Pig-Game/starter/script.js
@@ -15,14 +15,25 @@ const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
 // Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add("hidden");
+let scores, currentScore, playing, activePlayer;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+const initialize = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add("hidden");
+  player0El.classList.remove("player--winner");
+  player1El.classList.remove("player--winner");
+  player0El.classList.add("player--active");
+  player1El.classList.remove("player--active");
+};
 
 //switch to the next player
 const switchPlayer = function () {
@@ -80,3 +91,5 @@ btnHold.addEventListener("click", function () {
     }
   }
 });
+
+btnNew.addEventListener("click", initialize);
