4f91bb5cec99ab8f3b1e4baea2d2b170c4956e6c
Ahreum0714 <yalin7145@gmail.com>
2022-03-06 22:27:05 +0900

    A+B -4

--- baekjoon/01-입출력/js/10951.js
+++ baekjoon/01-입출력/js/10951.js
@@ -1,8 +1,8 @@
 // 백준 제출 시, readFileSync('/dev/stdin') 으로 변경할 것
 const fs = require("fs");
 const input = fs.readFileSync("예제.txt").toString().split("\n");
-const num = parseInt(input[0]);
-for (let i = 0; i < num; i++) {
-  let column = input[i + 1].split(" ");
+const num = input.length;
+for (let i = 0; i < num - 1; i++) {
+  let column = input[i].split(" ");
   console.log(parseInt(column[0]) + parseInt(column[1]));
 }
