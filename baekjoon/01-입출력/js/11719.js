d21fe007d966e62a065d71c90e66705c8cdbb846
Ahreum0714 <yalin7145@gmail.com>
2022-03-29 18:20:32 +0900

    그대로 출력하기 2

---
+++ baekjoon/01-입출력/js/11719.js
@@ -0,0 +1,7 @@
+// 백준 제출 시, readFileSync('/dev/stdin') 으로 변경할 것
+const fs = require("fs");
+const input = fs.readFileSync("예제.txt").toString().split("\n");
+
+for (let i in input) {
+  console.log(input[i]);
+}
