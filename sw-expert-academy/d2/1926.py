319db840013f9a3207216419b49d005f8dcde4f2
Ahreum0714 <yalin7145@gmail.com>
2022-05-16 05:02:28 +0900

    1926 - 간단한 369게임

---
+++ sw-expert-academy/d2/1926.py
@@ -0,0 +1,17 @@
+num = int(input())
+ans = ''
+condition = ['3','6','9']
+
+for n in range(1, num+1):
+    cnt = 0
+    digit = str(n)
+    for d in digit:
+        if d in condition:  cnt += 1
+
+    if cnt == 0: ans += digit
+    else: 
+        for j in range(0, cnt):
+            ans += '-'
+    ans += ' '
+
+print(ans)
