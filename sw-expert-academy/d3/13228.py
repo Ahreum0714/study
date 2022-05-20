5318119b9391a1b0569dbe3b85736262128b87e4
arh714 <99606073+Ahreum0714@users.noreply.github.com>
2022-05-20 19:07:30 +0900

    Create 13228.py

---
+++ sw-expert-academy/d3/13228.py
@@ -0,0 +1,8 @@
+T=int(input())
+for test_case in range(1, T+1):
+  S=input()
+  weekdays=["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"]
+  idx = weekdays.indexof(S)
+  left = 6 - idx
+  if left == 0: print(f'#{test_case} 7')
+  else: print(f'#{test_case} {left}'}
