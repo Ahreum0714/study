49cd64525416cc92a1ec034759c4e18e55c67555
Ahreum0714 <yalin7145@gmail.com>
2022-05-22 23:26:50 +0900

    [SWEA] D2 - 13229: 일요일(최종)

--- sw-expert-academy/d3/13229.py
+++ sw-expert-academy/d3/13229.py
@@ -4,8 +4,7 @@ sys.stdin = open("input.txt", "r")
 T = int(input())
 for test_case in range(1, T+1):
   S = input()
-  weekdays = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"]
+  weekdays = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"]
   idx = weekdays.index(S)
-  left = 6 - idx
-  if left == 0: print(f'#{test_case} 7')
-  else: print(f'#{test_case} {left}')
+  left = 7 - idx
+  print(f'#{test_case} {left}')
