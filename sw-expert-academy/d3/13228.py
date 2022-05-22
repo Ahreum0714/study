0c90b8ceb292435a0cdbe703cb0fb0e8598d737b
Ahreum0714 <yalin7145@gmail.com>
2022-05-22 23:16:10 +0900

    [SWEA] D2 - 13229: 일요일

--- sw-expert-academy/d3/13228.py
+++
@@ -1,8 +0,0 @@
-T=int(input())
-for test_case in range(1, T+1):
-  S=input()
-  weekdays=["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"]
-  idx = weekdays.index(S)
-  left = 6 - idx
-  if left == 0: print(f'#{test_case} 7')
-  else: print(f'#{test_case} {left}'}
---
+++ sw-expert-academy/d3/13229.py
@@ -0,0 +1,11 @@
+import sys
+sys.stdin = open("input.txt", "r")
+
+T = int(input())
+for test_case in range(1, T+1):
+  S = input()
+  weekdays = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"]
+  idx = weekdays.index(S)
+  left = 6 - idx
+  if left == 0: print(f'#{test_case} 7')
+  else: print(f'#{test_case} {left}')
