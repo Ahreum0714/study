cbdb10ec6aecfa7e4ef77b443a7f4c094e84f1e7
Ahreum0714 <yalin7145@gmail.com>
2022-06-02 16:13:09 +0900

    [OBJ] 스택 - 9012: 괄호

--- baekjoon/03-스택,큐/9012.py
+++ baekjoon/03-스택,큐/9012.py
@@ -1,1 +1,24 @@
+import sys
+sys.stdin = open("input.txt", "r")
+
 t = int(input())
+for _ in range(t):
+    stack = []
+    vps = ''
+    parenthesis = input()
+    for p in parenthesis:
+        if p == '(':
+            stack.append(p)
+        elif p == ')':
+            if len(stack) > 0:
+                stack.pop()
+            else:
+                vps += 'NO'
+                break
+    
+    if vps == '' and len(stack) == 0:
+        vps += 'YES'
+    elif vps == '' and len(stack) > 0:
+        vps += 'NO'
+    
+    print(vps)
