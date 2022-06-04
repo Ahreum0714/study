8a4d2411afea986adee8f03cff343c9e23b8f763
Ahreum0714 <yalin7145@gmail.com>
2022-06-04 23:56:31 +0900

    [OBJ] 스택 - 1874: 스택 수열 (주석 설명 추가)

--- baekjoon/03-스택,큐/1874.py
+++ baekjoon/03-스택,큐/1874.py
@@ -8,18 +8,21 @@ n = 1
 flag = 0
 for i in range(N):
     k = int(input())
+    # 입력한 수 k 를 만날 때 까지 오름차순으로 스택에 값을 push
     while k >= n:
         stack.append(n)
         ans.append('+')
         n += 1
     
+    # 스택의 top이 k 라면, pop 해서 수열 만들기
     if stack[-1] == k:
         stack.pop()
         ans.append('-')
     else:
-        print('NO')
-        flag = 1
-        break
+        # 스택의 top이 k가 아니라면, 스택으로 수열을 만들 수 없음
+        print('NO') # 스택은 top의 값만 빼낼 수 있기 때문.
+        flag = 1 # 스택의 top이 k보다 클 경우, 스택 속 k는 top 아래에 있음
+        break  # top을 빼기 전까지는 top 아래에 있는 k 값을 빼낼 수 없으므로, 수열을 완성할 수 없음
     
 if flag == 0:
     for a in ans:
