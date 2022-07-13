b049067eb462e5badba91d1ca909b13661a07f76
Ahreum0714 <yalin7145@gmail.com>
2022-07-13 14:56:52 +0900

    Nest.js with MongoDB - practice complete

--- nestjs-mongodb-practice/mongoose-practice/src/app.module.ts
+++ nestjs-mongodb-practice/mongoose-practice/src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    ProductsModule,
+    MongooseModule.forRoot(
+      // 'mongodb+srv://maximilian:B3dqPzooRLzFiVYm@cluster0-ntrwp.mongodb.net/nestjs-demo?retryWrites=true&w=majority',
+      'mongodb://localhost:27017/test2',
+      { useNewUrlParser: true },
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
