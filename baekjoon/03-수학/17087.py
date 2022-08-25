9d8ffd82e554e46d86339ed5693d88d4bc1f95ee
Ahreum0714 <yalin7145@gmail.com>
2022-08-25 18:05:10 +0900

    [OBJ] 수학 - 17087: 숨바꼭질6 (다른사람풀이 추가)

--- baekjoon/03-수학/17087.py
+++ baekjoon/03-수학/17087.py
@@ -1,20 +1,29 @@
-# 알고리즘 문제 풀이 https://www.acmicpc.net/problem/17087 숨바꼭질6 (132ms)
-import math
+# 알고리즘 문제 풀이 https://www.acmicpc.net/problem/17087 숨바꼭질6 (124ms)
+from math import gcd
 import sys
 sys.stdin = open('input.txt', 'r')
 input = sys.stdin.readline
 
 # 1. a 원소 각각마다 (s - a 원소)의 차를 모두 구한다
 # 2. 차들의 최대 공약수를 구한다
-n, s = map(int, input().strip().split())
-a = list(map(int, input().strip().split()))
+n, s = map(int, input().split())
+a = list(map(int, input().split()))
 
-if n == 1:
-    ans = abs(a[0] - s)
-else:
-    difference = [abs(s - a[i]) for i in range(len(a))]
-    ans = difference[0]
-    for i in range(1, len(difference)):
-        ans = math.gcd(ans, difference[i])
+difference = [abs(s-i) for i in a]
+ans = difference[0]
+for i in range(1, len(difference)):
+    ans = gcd(ans, difference[i])
 
 print(ans)
+
+'''
+----람다식을 활용한 다른 사람 풀이----
+from math import gcd
+N, S = map(int, input().split(" "))
+pos = list(map(lambda x: abs(int(x)-S), input().split()))
+print(gcd(*pos))
+----------------------------------
+    #python3.9부터 math.gcd()의 argument 개수가 2개 이상도 가능해짐! 
+    # 내 로컬 버전은 3.8.10이라 안됨..
+    # 참고: 백준 컴파일 버전은 3.10.4
+'''
