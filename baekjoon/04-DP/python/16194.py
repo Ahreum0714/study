45c76cc5d42caf7a8d59dabe4457b0e29e0f9109
Ahreum0714 <yalin7145@gmail.com>
2022-09-03 20:59:40 +0900

    [OBJ] DP - 16194: 카드 구매하기2

---
+++ baekjoon/04-DP/python/16194.py
@@ -0,0 +1,13 @@
+# https://www.acmicpc.net/problem/16194 카드 구매하기 2 (268ms)
+import sys
+sys.stdin = open('input.txt', 'r')
+input = sys.stdin.readline
+
+n = int(input())
+p = [0] + list(map(int, input().split()))
+dp = [0] + [10001 for _ in range(n)]
+dp[1] = p[1]
+for i in range(1, n+1):
+    for k in range(1, i+1):
+        dp[i] = min(dp[i], p[k] + dp[i-k])
+print(dp[n])
