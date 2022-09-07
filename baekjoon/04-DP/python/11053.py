640897696f24aaf8d6393a6f527a71a5571f0c45
Ahreum0714 <yalin7145@gmail.com>
2022-09-07 20:33:13 +0900

    [OBJ] DP - 11053: 가장 긴 증가하는 부분 수열(다른 풀이)

--- baekjoon/04-DP/python/11053.py
+++ baekjoon/04-DP/python/11053.py
@@ -8,8 +8,7 @@ a = list(map(int, input().split()))
 dp = [0] * n
 for i in range(n):
     for j in range(i):
-        if a[i] > a[j] and dp[i] < dp[j]: 
-            dp[i] = dp[j]
-    dp[i] += 1
+        if a[i] > a[j]: 
+            dp[i] = max(dp[i], dp[j]+1)
 
 print(max(dp))
