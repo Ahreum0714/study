236a2b7b020c8724eafaa8e97d9fb3457488916d
Ahreum0714 <yalin7145@gmail.com>
2022-09-07 20:58:19 +0900

    [OBJ] DP - 11053: 가장 긴 증가하는 부분 수열(주석 추가)

--- baekjoon/04-DP/python/11053.py
+++ baekjoon/04-DP/python/11053.py
@@ -5,7 +5,8 @@ input = sys.stdin.readline
 
 n = int(input())
 a = list(map(int, input().split()))
-dp = [0] * n
+dp = [1] * n
+# n의 크기가 1e3 이기 때문에 dp로 풀어도 됨
 for i in range(n):
     for j in range(i):
         if a[i] > a[j]: 
