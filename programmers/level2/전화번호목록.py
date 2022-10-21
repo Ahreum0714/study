ad77f522090a2c759def50ddded4b7d5ff46199a
arh714 <99606073+Ahreum0714@users.noreply.github.com>
2022-10-21 13:31:16 +0900

    [Prog] 해시 - lv2: 전화번호 목록

---
+++ programmers/level2/전화번호목록.py
@@ -0,0 +1,14 @@
+def solution(phone_book):
+    answer = True
+    hash_map = {}
+    for phone_num in phone_book:
+        hash_map[phone_num] = 1
+    
+    for phone_num in phone_book:
+        tmp = ''
+        for num in phone_num:
+            tmp += num
+            if tmp in hash_map and tmp != phone_num:
+                answer = False
+                
+    return answer
