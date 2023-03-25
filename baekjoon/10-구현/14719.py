b3c14c6d4ecff93226dc2e3aecccca901bd235b0
Ahreum0714 <yalin7145@gmail.com>
2023-03-26 06:27:56 +0900

    [OBJ] 구현 - 14719: 빗물

---
+++ baekjoon/10-구현/14719.py
@@ -0,0 +1,19 @@
+# https://www.acmicpc.net/problem/14719 빗물 (44ms)
+import sys
+sys.stdin = open('input.txt', 'r')
+input = sys.stdin.readline
+
+H, W = map(int, input().split())
+blocks = list(map(int, input().split()))
+total = 0
+
+for i in range(1, W-1):
+    left_max = max(blocks[:i])
+    right_max = max(blocks[i+1:])
+
+    compare = min(left_max, right_max)
+
+    if blocks[i] < compare:
+        total += compare - blocks[i]
+
+print(total)
